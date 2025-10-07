93b1c944f5143d6d157cf4efbb242c216d438034
Maksym Yasiura <m.yasyura@gmail.com>
2025-10-08 02:39:48 +0300

    Code cleanup (deleted many comments)

--- Extract_Data_From_sites.py
+++ Extract_Data_From_sites.py
@@ -33,86 +33,24 @@ def extractor(bank, link, container, names, buy, sale, doBefore = None):
     saveToCSV(bank, currencyNames, currencyBuy, currencySale)
 
 #ПУМБ
-extractor("Pumba", "https://about.pumb.ua/info/currency_converter",
+extractor("PUMB", "https://about.pumb.ua/info/currency_converter",
           "#single-page-rate-1 tbody",
           "tr td:first-child",
           "tr td:nth-child(2)",
           "tr td:last-child"
           )
 #Ощад банк
-extractor("Zaochadluvui", "https://www.oschadbank.ua/currency-rate",
+extractor("OschadBank", "https://www.oschadbank.ua/currency-rate",
           "table.heading-block-currency-rate__table tbody",
           "tr td:nth-child(2) span",
           "tr td:nth-child(4) span",
           "tr td:nth-child(5) span"
           )
 
-extractor("Priv", "https://privatbank.ua/obmin-valiut",
+extractor("PrivateBank", "https://privatbank.ua/obmin-valiut",
           "#swift-currency-block .table_block table.table_show tbody",
           ".tbody-swift .currency-swift",
           ".buy-swift",
           ".sale-swift",
           lambda : driver.find_element(By.ID, "swift-block").send_keys(Keys.RETURN)
           )
-
-
-# driver.get("https://privatbank.ua/obmin-valiut")
-#
-# # click SWIFT
-# driver.find_element(By.ID, "swift-block").send_keys(Keys.RETURN)
-#
-# # get table
-# currencyTable_d = driver.find_element(By.CSS_SELECTOR, "#swift-currency-block .table_block table.table_show tbody")
-#
-# currencyNames_d = currencyTable_d.find_elements(By.CSS_SELECTOR, ".tbody-swift .currency-swift")
-# currencyBuy_d = currencyTable_d.find_elements(By.CSS_SELECTOR, ".buy-swift")
-# currencySale_d = currencyTable_d.find_elements(By.CSS_SELECTOR, ".sale-swift")
-#
-# currencyNames = [name.get_attribute("innerHTML")[0:3] for name in currencyNames_d];
-# currencyBuy = [price.get_attribute("innerHTML") for price in currencyBuy_d];
-# currencySale = [price.get_attribute("innerHTML") for price in currencySale_d];
-#
-# saveToCSV("Privatik", currencyNames, currencyBuy, currencySale)
-#
-#
-# # pyplot.plot(currencyBuy)
-# # pyplot.plot(currencySale)
-# # pyplot.grid()
-# #
-# # pyplot.show()
-#
-# #driver.close()
-#
-#
-# #ПУМБ
-#
-# # driver.get("https://about.pumb.ua/info/currency_converter")
-# #
-# # currencyTable_d = driver.find_element(By.CSS_SELECTOR, "#single-page-rate-1 tbody")
-# #
-# # currencyNames_d = currencyTable_d.find_elements(By.CSS_SELECTOR, "tr td:first-child")
-# # currencyBuy_d = currencyTable_d.find_elements(By.CSS_SELECTOR, "tr td:nth-child(2)")
-# # currencySale_d = currencyTable_d.find_elements(By.CSS_SELECTOR, "tr td:last-child")
-# #
-# # for i in currencySale_d:
-# #     print(i.get_attribute("innerHTML"))
-# #
-# # currencyNames = [name.get_attribute("innerHTML")[0:3] for name in currencyNames_d];
-# # currencyBuy = [price.get_attribute("innerHTML") for price in currencyBuy_d];
-# # currencySale = [price.get_attribute("innerHTML") for price in currencySale_d];
-#
-#
-# # saveToCSV("PUMB", currencyNames, currencyBuy, currencySale)
-#
-# # driver.get("https://www.oschadbank.ua/currency-rate")
-# #
-# # currencyTable_d = driver.find_element(By.CSS_SELECTOR, "table.heading-block-currency-rate__table tbody")
-# # currencyNames_d = currencyTable_d.find_elements(By.CSS_SELECTOR, "tr td:nth-child(2) span")
-# # currencyBuy_d = currencyTable_d.find_elements(By.CSS_SELECTOR, "tr td:nth-child(4) span")
-# # currencySale_d = currencyTable_d.find_elements(By.CSS_SELECTOR, "tr td:nth-child(5) span")
-# #
-# # currencyNames = [name.get_attribute("innerHTML")[0:3] for name in currencyNames_d];
-# # currencyBuy = [price.get_attribute("innerHTML") for price in currencyBuy_d];
-# # currencySale = [price.get_attribute("innerHTML") for price in currencySale_d];
-# #
-# # saveToCSV("Oschad", currencyNames, currencyBuy, currencySale)
