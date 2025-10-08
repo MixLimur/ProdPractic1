9dc589ef7ff1dcdf3f4d92d0ecfb8b273cecd2fc
Maksym Yasiura <m.yasyura@gmail.com>
2025-10-09 01:16:35 +0300

    refactor code, divide previous extract function to smaller ones and union them in extractData function. Also first tris to apply new saving format

--- Extract_Data_From_sites.py
+++ Extract_Data_From_sites.py
@@ -6,6 +6,7 @@ from datetime import date
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(5)
+global_list = []
 
 def saveToCSV(bankName, curName, curBuy, curSale):
     with open(f"./Exchanges_CSV/{bankName}_{date.today().strftime("%d_%m_%Y")}.csv", "w", encoding="UTF-8") as f:
@@ -14,43 +15,62 @@ def saveToCSV(bankName, curName, curBuy, curSale):
             csv_format = f"{curName[i]},{curBuy[i]},{curSale[i]}\n"
             f.write(csv_format)
 
+def saveToOneCSV():
 
-def extractor(bank, link, container, names, buy, sale, doBefore = None):
+    with open(f"./Exchanges_CSV/Exchanges.csv", "a", encoding="UTF-8") as f:
+        f.write("Name,buy,sale,bank,date\n")
+        for i in range(len(global_list)):
+            csv_format = (f"{global_list[i][0]},{global_list[i][1]},"
+                          f"{global_list[i][2]},{global_list[i][3]},{global_list[i][4]}\n")
+            f.write(csv_format)
+
+def listToStr(lst, lengthLimit = None):
+     lst[:] = [item.get_attribute("innerHTML")[:lengthLimit] for item in lst];
+
+def extractFromSite(driver, selectorsDictionary):
+    currencyTable = driver.find_element(By.CSS_SELECTOR, selectorsDictionary["container"])
+    currencyNames = currencyTable.find_elements(By.CSS_SELECTOR, selectorsDictionary["names"])
+    currencyBuy = currencyTable.find_elements(By.CSS_SELECTOR, selectorsDictionary["buy"])
+    currencySale = currencyTable.find_elements(By.CSS_SELECTOR, selectorsDictionary["sale"])
+    return currencyNames, currencyBuy, currencySale
+
+def extractData(bank, link, container, names, buy, sale, doBefore = None):
     driver.get(link)
 
     if doBefore != None:
         doBefore()
 
-    currencyTable_d = driver.find_element(By.CSS_SELECTOR, container)
-    currencyNames_d = currencyTable_d.find_elements(By.CSS_SELECTOR, names)
-    currencyBuy_d = currencyTable_d.find_elements(By.CSS_SELECTOR, buy)
-    currencySale_d = currencyTable_d.find_elements(By.CSS_SELECTOR, sale)
+    selectorsDictionary = {"container": container, "names":names, "buy":buy, "sale":sale}
+    currencyNames, currencyBuy, currencySale = extractFromSite(driver, selectorsDictionary)
 
-    currencyNames = [name.get_attribute("innerHTML")[0:3] for name in currencyNames_d];
-    currencyBuy = [price.get_attribute("innerHTML") for price in currencyBuy_d];
-    currencySale = [price.get_attribute("innerHTML") for price in currencySale_d];
+    listToStr(currencyNames, 3)
+    listToStr(currencyBuy)
+    listToStr(currencySale)
+
+    for name, buy, sale in zip(currencyNames, currencyBuy, currencySale):
+        global_list.append([name, buy, sale, bank, date.today().strftime("%d_%m_%Y")])
 
     saveToCSV(bank, currencyNames, currencyBuy, currencySale)
 
 #ПУМБ
-extractor("PUMB", "https://about.pumb.ua/info/currency_converter",
+extractData("PUMB", "https://about.pumb.ua/info/currency_converter",
           "#single-page-rate-1 tbody",
           "tr td:first-child",
           "tr td:nth-child(2)",
           "tr td:last-child"
-          )
+            )
 #Ощад банк
-extractor("OschadBank", "https://www.oschadbank.ua/currency-rate",
+extractData("OschadBank", "https://www.oschadbank.ua/currency-rate",
           "table.heading-block-currency-rate__table tbody",
           "tr td:nth-child(2) span",
           "tr td:nth-child(4) span",
           "tr td:nth-child(5) span"
-          )
+            )
 
-extractor("PrivateBank", "https://privatbank.ua/obmin-valiut",
+extractData("PrivateBank", "https://privatbank.ua/obmin-valiut",
           "#swift-currency-block .table_block table.table_show tbody",
           ".tbody-swift .currency-swift",
           ".buy-swift",
           ".sale-swift",
-          lambda : driver.find_element(By.ID, "swift-block").send_keys(Keys.RETURN)
-          )
+            lambda : driver.find_element(By.ID, "swift-block").send_keys(Keys.RETURN)
+            )
